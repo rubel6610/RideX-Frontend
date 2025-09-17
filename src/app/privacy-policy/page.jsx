bf9afa157d5a26b78fdfb6dbd314c877e643b7ec
omarfaruk-dev <omarfaruk.dev@gmail.com>
2025-09-17 23:43:55 +0600

    privacy policy route created

---
+++ src/app/privacy-policy/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const privacyPolicy = () => {
+    return (
+        <div>
+            <h1>Privacy policy</h1>
+        </div>
+    );
+};
+
+export default privacyPolicy;
