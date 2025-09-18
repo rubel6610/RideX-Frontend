2743af37bd9c890240116d1475803499dc58dd64
ratulislam46 <arfanratul46@gmail.com>
2025-09-18 20:42:27 +0600

    Added register page title

---
+++ src/app/register/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function RegisterPage() {
+  return (
+    <div className='mt-20'>
+      {/* title and logo  */}
+      <div>
+        <h2>Join RedeX</h2>
+        <p>Create your account to get started</p>
+      </div>
+    </div>
+  )
+}
+
+export default RegisterPage
