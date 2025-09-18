29f2401e56788742bb3ef16a1c65e84839881857
ratulislam46 <arfanratul46@gmail.com>
2025-09-18 20:21:18 +0600

    Added register page

---
+++ src/app/register/component/RegisterForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RegisterForm = () => {
+    return (
+        <div className='mt-20'>
+            <h1>Register Forum</h1>
+        </div>
+    );
+};
+
+export default RegisterForm;
