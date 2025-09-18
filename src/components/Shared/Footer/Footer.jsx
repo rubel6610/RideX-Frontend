85dc7ae7194d369e354a83de2874089f01595375
Si Fad <sifayed00@gmail.com>
2025-09-18 20:47:22 +0600

    i update logo for both dark and light theme and also added a logo in footer.

--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -1,31 +1,40 @@
-// components/Footer.jsx
 "use client";
 import Link from "next/link";
 import { Facebook, Linkedin, Twitter, Youtube } from 'lucide-react';
+import logo from '../../../Assets/logo-dark.webp';
+import Image from "next/image";
 
 
 export default function Footer() {
   return (
     <footer className="bg-black border-t text-white px-5 sm:px-6 xl:px-28 ">
-      <div className="max-w-7xl mx-auto px-6 py-10 grid grid-cols-1 md:grid-cols-5 gap-8">
+      <div className="max-w-7xl mx-auto py-10 grid grid-cols-1 md:grid-cols-5 gap-8">
         {/* Left Section */}
         <div className="md:col-span-2">
-          <h2 className="text-3xl font-semibold text-primary">Start your free trial today</h2>
-          <div className="mt-4 text-sm">
-            <p className="">
-              <strong>Call Us</strong>
-            </p>
-            <p className="text-gray-300">UK: +44 1223 567 636</p>
-            <p className="text-gray-300">USA: +1 (855) 976-2571</p>
+          <div className="flex flex-col gap-3">
+            <Link
+              href="/"
+              className="text-xl md:text-2xl leading-0 font-bold font-oxygen text-[var(--primary)]"
+            >
+              <Image
+                src={logo}
+                alt="RideX Logo"
+                width={120}
+                height={50}
+                className="object-contain"
+              />
+            </Link>
+            <h2 className="text-2xl font-semibold text-primary leading-6.5">Start your journey with safety.</h2>
           </div>
+
           {/* Logo + Social */}
-          <div className="mt-6">
-            <p className="text-xl font-bold">outreachPlus</p>
-            <div className="flex gap-3 mt-3 text-gray-500">
-              <Facebook className="cursor-pointer hover:text-blue-600" />
-              <Linkedin className="cursor-pointer hover:text-blue-700" />
-              <Twitter className="cursor-pointer hover:text-sky-500" />
-              <Youtube className="cursor-pointer hover:text-red-500" />
+          <div className="mt-3">
+            <strong>outreachPlus</strong>
+            <div className="flex gap-2 mt-1 text-gray-500">
+              <Facebook className="cursor-pointer hover:text-blue-600 w-5.5" />
+              <Linkedin className="cursor-pointer hover:text-blue-700 w-5.5" />
+              <Twitter className="cursor-pointer hover:text-sky-500 w-5.5" />
+              <Youtube className="cursor-pointer hover:text-red-500 w-5.5" />
             </div>
           </div>
         </div>
@@ -45,9 +54,6 @@ export default function Footer() {
             </li>
             <li>
               <Link href="#" className="hover:underline">About us</Link>
-            </li>
-            <li>
-              <Link href="#" className="hover:underline">Terms & Conditions</Link>
             </li>
           </ul>
         </div>
@@ -86,10 +92,10 @@ export default function Footer() {
       </div>
 
       {/* Bottom Bar */}
-      <div className="border-t text-center text-xs py-4 text-gray-400 flex flex-col md:flex-row items-center justify-between px-6">
+      <div className="border-t sm:text-center text-xs py-4 text-gray-400 flex flex-col sm:flex-row sm:items-center sm:justify-between px-6">
         <p>©2025 RIdeX Technologies. All rights reserved</p>
         <div className="flex gap-4 mt-2 md:mt-0">
-          <Link href="#" className="hover:underline">Terms of Service</Link>
+          <Link href="#" className="hover:underline">Terms & Conditions</Link>
           <Link href="/privacy-policy" className="hover:underline">Privacy Policy</Link>
         </div>
       </div>
