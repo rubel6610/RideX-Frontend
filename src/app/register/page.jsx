0a316f95d449ee6badb8f59c86452954be90ea72
ratulislam46 <arfanratul46@gmail.com>
2025-09-18 23:58:55 +0600

    Added label for each input field

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 function RegisterPage() {
 
 
-  
+
   return (
     <div className='mt-20 container mx-auto'>
 
@@ -15,24 +15,31 @@ function RegisterPage() {
       </div>
 
       {/* register page from  */}
-      <form className='shadow-lg p-8 rounded-xl space-y-4'>
-
-
-
+      <form className='shadow-lg p-8 rounded-xl space-y-4 max-w-2xl mx-auto'>
         {/* first & last name field  */}
-        <div className='space-y-4 grid grid-cols-1 md:grid-cols-2 gap-0 md:gap-4'>
-          <Input></Input>
-          <Input></Input>
+        <div className='space-y-2 grid grid-cols-1 md:grid-cols-2 gap-0 md:gap-4'>
+          <div>
+            <label>First Name</label>
+            <Input></Input>
+          </div>
+          <div>
+            <label>Last name</label>
+            <Input></Input>
+          </div>
         </div>
         {/* email field  */}
+        <label>Email</label>
         <Input></Input>
         {/* phone number field  */}
+        <label>Phone Number</label>
         <Input></Input>
         {/* password field  */}
+        <label>Password</label>
         <Input></Input>
         {/* confirm password field  */}
+        <label>Confirm Password</label>
         <Input></Input>
-        
+
 
       </form>
     </div>
