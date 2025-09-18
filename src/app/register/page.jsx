38c848134018debf39330aa62edc08db57e25eb5
ratulislam46 <arfanratul46@gmail.com>
2025-09-19 02:05:46 +0600

    modify register form and added toogle button

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -3,6 +3,7 @@ import { Input } from '@/components/ui/input'
 import CarLogo from '../../Assets/car-icon.png'
 import Image from 'next/image'
 import React from 'react'
+import Link from 'next/link'
 
 function RegisterPage() {
 
@@ -10,7 +11,7 @@ function RegisterPage() {
     <div className='mt-28 container mx-auto mb-16'>
 
       {/* title and logo  */}
-      <div className='flex flex-col items-center space-y-2 my-10'>
+      <div className='flex flex-col items-center space-y-2 my-6'>
         <Image
           src={CarLogo}
           alt="car-logo"
@@ -24,7 +25,7 @@ function RegisterPage() {
 
       {/* register page from  */}
       <form className='shadow-lg p-8 rounded-xl space-y-3 max-w-xl mx-auto border border-muted-foreground/10'>
-        <p className='text-muted-foreground text-center'>Sign up as a driver to earn money by providing rides</p>
+        <p className='text-muted-foreground text-center mb-8'>Sign up as a driver to earn money by providing rides</p>
 
         {/* first & last name field  */}
         <div className='space-y-2 grid grid-cols-1 md:grid-cols-2 gap-0 md:gap-4'>
@@ -40,7 +41,7 @@ function RegisterPage() {
 
         {/* email field  */}
         <label>Email</label>
-        <Input type="email" placeholder="your@email.com"></Input>
+        <Input type="email" placeholder="your@email.com" ></Input>
 
         {/* phone number field  */}
         <label>Phone Number</label>
@@ -57,12 +58,14 @@ function RegisterPage() {
         <input type="checkbox" /> <small className='text-muted-foreground'>
           I agree to the <span className='text-primary'>Terms of Service</span> and <span className='text-primary'>Privacy Policy</span>
         </small>
-
+        {/* Create account button  */}
         <Button variant="primaryBtn" className="w-full">Create Account</Button>
         {/* google register button  */}
         <Button variant="outline" className="w-full"> Continue with Google</Button>
         {/* facebook register button  */}
         <Button variant="outline" className="w-full"> Continue with Facebook</Button>
+        {/* toggle sign in page  */}
+        <p className='text-center text-muted-foreground'>Already have an account? <span className='text-primary'><Link href='/signin'/>Sign in</span></p>
       </form>
     </div>
   )
