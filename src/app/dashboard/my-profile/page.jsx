278bff2b714b1321b9349aa6447e50bb506d92cb
omarfaruk-dev <omarfaruk.dev@gmail.com>
2025-09-25 00:48:28 +0600

    added: my-profile route

---
+++ src/app/dashboard/my-profile/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MyProfile = () => {
+    return (
+        <div>
+            <h2 className="text-2xl font-bold mb-4 text-foreground">My Profile</h2>
+            <p>All of my profile info here.</p>
+        </div>
+    );
+};
+
+export default MyProfile;
