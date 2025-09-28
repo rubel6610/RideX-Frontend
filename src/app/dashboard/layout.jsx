33b7301809e50b39cfeb93727886e11085b329d4
MD RUBEL HOSEN <rubelhosen1310@gmail.com>
2025-09-28 19:50:59 +0600

    modified layout.jsx dashboard

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -52,7 +52,7 @@ export default function DashboardLayout({ children }) {
     fetchUserData();
   }, [user?.email]);
 
-  const userRole = "admin";
+  const userRole = "user";
 
   return (
     <ProtectedRoute>
